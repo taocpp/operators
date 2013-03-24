7572e176e027e365dc4169b80da658bd6082896c
Daniel Frey <d.frey@gmx.de>
2013-03-24 17:22:24 +0100

    Updated header to include license information

--- include/df/commutative_addable.hpp
+++ include/df/commutative_addable.hpp
@@ -1,5 +1,8 @@
-// The operators library
-// Copyright (c) 2013 Daniel Frey
+// The Operators library
+
+// Copyright Daniel Frey 2013
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef DF_INCLUDE_COMMUTATIVE_ADDABLE_HPP
 #define DF_INCLUDE_COMMUTATIVE_ADDABLE_HPP
