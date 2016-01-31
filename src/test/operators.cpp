9db9fa812f0bf14ba341d93b898383ea31ecc9f8
Daniel Frey <d.frey@gmx.de>
2016-01-31 12:05:46 +0100

    Private inheritance is enough

--- src/test/operators.cpp
+++ src/test/operators.cpp
@@ -5,14 +5,13 @@
 
 #include <cassert>
 
-struct X
+class X
   : tao::operators::ordered_field< X >,
     tao::operators::modable< X >,
     tao::operators::ordered_field< X, int >,
     tao::operators::modable< X, int >
 {
-  int v_;
-
+public:
   explicit X( const int v ) noexcept : v_( v ) {}
 
   X( const X& x ) noexcept : v_( x.v_ ) {}
@@ -28,6 +27,8 @@ struct X
   X& operator*=( const int v ) { v_ *= v; return *this; }
   X& operator/=( const int v ) { v_ /= v; return *this; }
   X& operator%=( const int v ) { v_ %= v; return *this; }
+
+  int v_;
 };
 
 bool operator==( const X& lhs, const X& rhs ) { return lhs.v_ == rhs.v_; }
