8564890ad4e8e1bd9d28c9d41f5c6784ae43d2d0
Daniel Frey <d.frey@gmx.de>
2017-02-09 22:55:42 +0100

    Disable is_empty check for Windows due to inefficient ABI

--- src/test/operators/operators.cpp
+++ src/test/operators/operators.cpp
@@ -1,5 +1,5 @@
 // The Art of C++ / Operators
-// Copyright (c) 2013-2016 Daniel Frey
+// Copyright (c) 2013-2017 Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/operators/
 
 #include <tao/operators.hpp>
@@ -114,7 +114,9 @@ int main()
   assert( x3 % x2 == 1 );
   assert( x3 % 2 == 1 );
 
+#ifndef _WIN32
   static_assert( std::is_empty< E >::value, "oops" );
+#endif
 
   adl_test( E() );
 
