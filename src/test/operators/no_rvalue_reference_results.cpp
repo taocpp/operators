c593229859795e4965e0bc80147f9b308986fbe8
Daniel Frey <d.frey@gmx.de>
2018-02-13 22:28:02 +0100

    Fix name of define

--- src/test/operators/no_rvalue_reference_results.cpp
+++ src/test/operators/no_rvalue_reference_results.cpp
@@ -2,5 +2,5 @@
 // Copyright (c) 2013-2018 Daniel Frey
 // Please see LICENSE for license or visit https://github.com/taocpp/operators/
 
-#define TAO_NO_RVALUE_REFERENCE_RESULTS
+#define TAO_OPERATORS_NO_RVALUE_REFERENCE_RESULTS
 #include "operators.cpp"
