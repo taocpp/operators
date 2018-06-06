30c3e1f4a978dd04ce17f61f08b261dd198bcc05
Daniel Frey <d.frey@gmx.de>
2018-06-06 18:20:41 +0200

    Update

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,8 @@
 from conans import ConanFile, CMake
 import os
 
-class TAOCPPOperatorsConan(ConanFile):
-    name = "taocpp-operators"
+class OperatorsConan(ConanFile):
+    name = "operators"
     description = "C++11 single-header library that provides highly efficient, move aware operators for arithmetic data types"
     homepage = "https://github.com/taocpp/operators"
     url = homepage
@@ -14,7 +14,6 @@ class TAOCPPOperatorsConan(ConanFile):
         cmake = CMake(self)
 
         cmake.definitions["TAOCPP_OPERATORS_BUILD_TESTS"] = "OFF"
-        cmake.definitions["TAOCPP_OPERATORS_BUILD_EXAMPLES"] = "OFF"
         cmake.definitions["TAOCPP_OPERATORS_INSTALL_DOC_DIR"] = "licenses"
 
         cmake.configure()
